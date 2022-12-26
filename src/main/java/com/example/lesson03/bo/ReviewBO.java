d7975e6278053fa0b7fd157ec359c4278831d467
test <test@test.com>
2022-12-26 18:40:56 +0900

    <Spring> insert 예제

--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -10,9 +10,24 @@ import com.example.lesson03.model.Review;
 public class ReviewBO {
 	
 	@Autowired
-	private ReviewDAO reviewdao;
+	private ReviewDAO reviewDAO;
 
 	public Review getReview(int id) { // null이 들어가도 되면 Integer, id를 필수로 넘겨야함
-		return reviewdao.selectReview(id);
+		return reviewDAO.selectReview(id);
+	}
+	
+	// input: Review 객체
+	// output: int(insert가 성공된 행 수)
+	public int addReview(Review review) {
+		return reviewDAO.insertReview(review);
+	}
+	
+	// addReviewAsField(4, "콤비네이션R", "신바다", 5.0, "역시 맛있다!")
+	// point는 null허용이기 때문에 Double로 해도됨
+	public int addReviewAsField(
+			int storeId, String menu, String userName, Double point, String review
+			) {
+		return reviewDAO.insertReviewAsField(storeId, menu, userName, point, review);
+		// 자동완성 꼭 다시 확인하기(정확하지 않을 수 있음)
 	}
 }
--- src/main/java/com/example/lesson03/dao/ReviewDAO.java
+++ src/main/java/com/example/lesson03/dao/ReviewDAO.java
@@ -1,5 +1,6 @@
 package com.example.lesson03.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.lesson03.model.Review;
@@ -8,4 +9,14 @@ import com.example.lesson03.model.Review;
 public interface ReviewDAO {
 	public Review selectReview(int id); // BO의 메소드를 복붙하고 select로 변경하기
 	// 파라미터가 한 개면 @Param("id")이 부분 없애도됨
+	
+	public int insertReview(Review review);
+	
+	public int insertReviewAsField(
+			// 하나의 맵으로 구성됨(@Param) - 두 개 이상일 경우
+			@Param("storeId") int storeId, 
+			@Param("menu") String menu, 
+			@Param("userName") String userName, 
+			@Param("point") Double point, 
+			@Param("review") String review);
 }
